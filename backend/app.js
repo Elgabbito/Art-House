fb2c49f7b9c8589507390f1bcf9471573a221b58
Omotunde Gabriel <elgabbito123@gmail.com>
2023-10-31 11:37:24 +0100

    login routes

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,31 @@
-require("dotenv").config();
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const userAuthRoutes = require("./routes/userAuth");
+const userloginRoutes = require("./routes/userlogin")
+
+
+// Add routes and middleware to server
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use("/auth", userAuthRoutes);
+app.use("/login", userloginRoutes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  return next(err);
+});
+
+// development error handler will print stacktrace
+if (app.get("env") === "development") {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    return res.json({ message: err.message, error: err });
+  });
+}
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000!");
+});
