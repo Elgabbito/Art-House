d33e3aa5732c8f372e44531109a184d26ff77706
Marnin-A <msmaudu@gmail.com>
2023-11-01 11:29:04 +0100

    Untracking changes made to db/index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,7 +1,7 @@
 const { Client } = require("pg");
 require("dotenv").config();
 const client = new Client({
-  user: "user",
+  user: "marnin_a",
   host: "localhost",
   database: "savanna-showcase",
   password: process.env.password,
