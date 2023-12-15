b955a0f2dd30438865798172ecd0e82519c44cdd
Marnin-A <msmaudu@gmail.com>
2023-12-15 17:53:17 +0100

    refactor: Change db client to use connection string

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
 const { Client } = require("pg");
+const connectionString = process.env.DATABASE_URL;
 const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+	connectionString,
 });
 
 client.connect();
